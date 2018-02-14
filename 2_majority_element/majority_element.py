00fee3231365964db6a37f97d2d08c85af8ee266
FaiBarazi <f.barazi@gmail.com>
2018-02-13 20:51:23 -0500

    Implement get majority in O(n) time

--- 2_majority_element/majority_element.py
+++ 2_majority_element/majority_element.py
@@ -1,18 +1,22 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
+def get_majority_element(a, right):
+    counter = {}
+    majority = right // 2
+    for i in a:
+        if i in counter:
+            counter[i] += 1
+        else:
+            counter[i] = 1
+        if counter[i] > majority:
+            return i
     return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, n) != -1:
         print(1)
     else:
         print(0)
