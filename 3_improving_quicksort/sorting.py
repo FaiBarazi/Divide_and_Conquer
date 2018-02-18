e9cc0727c8e3fa477c65786c995eb2b9afea1b8a
FaiBarazi <f.barazi@gmail.com>
2018-02-17 20:13:43 -0500

    Implement 3 way quicksort Dijkstra style

--- 3_improving_quicksort/sorting.py
+++ 3_improving_quicksort/sorting.py
@@ -2,13 +2,34 @@
 import sys
 import random
 
+
 def partition3(a, l, r):
-    #write your code here
-    pass
+    """Three way parittion for faster sorting of lists with repeated elemenets.
+
+    param:
+    a: list - Unsorted elements
+    l: int - index of first value of the array
+    r: int - index of last value of the array
+    """
+    pivot = a[l]
+    i = l
+    while i <= r:
+        if a[i] < pivot:
+            a[l], a[i] = a[i], pivot
+            l += 1
+            i += 1
+        elif a[i] > pivot:
+            temp_i = a[i]
+            a[i] = a[r]
+            a[r] = temp_i
+            r -= 1
+        else:
+            i += 1
+    return a, l, r
 
 def partition2(a, l, r):
     x = a[l]
-    j = l;
+    j = l
     for i in range(l + 1, r + 1):
         if a[i] <= x:
             j += 1
@@ -22,11 +43,10 @@ def randomized_quick_sort(a, l, r):
         return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
-
+    a, m1, m2 = partition3(a, l, r)
+    print (a, m1, m2)
+    randomized_quick_sort(a, l, m1 - 1)
+    randomized_quick_sort(a, m2 + 1, r)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
